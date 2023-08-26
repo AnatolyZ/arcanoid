f1f9fdbb4744c68ad143b11721c9fd6bb59c722e
Anatoly Zharkov <amzharkov@gmail.com>
2023-08-27 00:14:29 +0300

    spawn bricks according to loaded ldtk level.

--- src/brick/mod.rs
+++ src/brick/mod.rs
@@ -2,11 +2,14 @@ use bevy::prelude::*;
 
 mod components;
 mod systems;
+
 pub struct BricksPlugin;
 
 impl Plugin for BricksPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, systems::spawn_bricks_array)
-            .add_systems(Update, systems::collision_handler);
+        app.add_systems(
+            Update,
+            (systems::collision_handler, systems::spawn_bricks_ldtk),
+        );
     }
 }
