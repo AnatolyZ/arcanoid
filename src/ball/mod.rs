ab191a3ef802f7dcc0ceed0f7ef4359e6f1d6e2a
Anatoly Zharkov <amzharkov@gmail.com>
2023-09-08 16:59:57 +0300

    speed limits changed. Limit is calculated fot overral length.

--- src/ball/mod.rs
+++ src/ball/mod.rs
@@ -4,11 +4,6 @@ mod systems;
 use bevy::prelude::*;
 pub use components::Ball;
 
-const MAX_X_SPEED: f32 = 300.0;
-const MAX_Y_SPEED: f32 = 300.0;
-const MIN_X_SPEED: f32 = 100.0;
-const MIM_Y_SPEED: f32 = 100.0;
-
 pub struct BallPlugin;
 
 impl Plugin for BallPlugin {
