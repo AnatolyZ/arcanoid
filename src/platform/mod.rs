65da1ecd0be61bc6a2ad63250d53e08f9f82221c
Anatoly Zharkov <amzharkov@gmail.com>
2023-10-13 21:59:42 +0300

    States added. Pause implemented.

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -6,9 +6,14 @@ mod systems;
 pub use components::Platform;
 pub struct PlatformPlugin;
 
+use crate::states::GameState;
+
 impl Plugin for PlatformPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, systems::spawn_platform)
-            .add_systems(Update, systems::move_platform);
+            .add_systems(
+                Update,
+                systems::move_platform.run_if(in_state(GameState::Game)),
+            );
     }
 }
