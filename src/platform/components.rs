bc18dd272668f9ff30cf0aaa2259b43b80326bdd
Anatoly Zharkov <amzharkov@gmail.com>
2024-09-26 00:07:38 +0400

    Platform spawning reworked, platform-ball collision sound added.

--- src/platform/components.rs
+++ src/platform/components.rs
@@ -2,5 +2,5 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Platform {
-    pub length: u32, // platform length in tiles (minumum 2 tiles)
+    pub collision_sound: Handle<AudioSource>,
 }
--- src/platform/systems.rs
+++ src/platform/systems.rs
@@ -3,6 +3,7 @@ use crate::ball::components::Ball;
 use crate::ball::NewBallOnPlatform;
 use crate::play_area::MainCamera;
 use crate::textures::{resources::Textures, HALF_TILE_SIZE, TILE_SIZE};
+use crate::SCREEN_HEIGHT;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy_rapier2d::prelude::*;
@@ -13,31 +14,15 @@ const PLATFORM_MIDDLE_TILE: usize = 5;
 const PLATFORM_LAST_TILE: usize = 6;
 const BALL_SPRITE_RADIUS: f32 = 16.0;
 
-fn get_plarform_center_x_offset(platform_length: u32) -> f32 {
-    platform_length as f32 * TILE_SIZE / 2.0 - HALF_TILE_SIZE
-}
-
-pub fn spawn_platform(mut commands: Commands, windows: Query<&Window>, textures: Res<Textures>) {
-    let window = windows.single();
-
-    let center_offset = get_plarform_center_x_offset(PLATFORM_DEFAULT_LENGTH) * -1f32;
+pub fn spawn_platform(
+    mut commands: Commands,
+    textures: Res<Textures>,
+    asset_server: Res<AssetServer>,
+) {
     let platform_entity = commands
         .spawn((
             Platform {
-                length: PLATFORM_DEFAULT_LENGTH,
-            },
-            SpriteBundle {
-                texture: textures.industrial.texture.clone(),
-                transform: Transform::from_xyz(
-                    center_offset,
-                    -window.height() / 2.0 + TILE_SIZE,
-                    1.0,
-                ),
-                ..Default::default()
-            },
-            TextureAtlas {
-                layout: textures.industrial.layout.clone(),
-                index: PLATFORM_FIRST_TILE,
+                collision_sound: asset_server.load("sounds/platform.ogg"),
             },
             RigidBody::Dynamic,
             ExternalImpulse {
@@ -51,18 +36,28 @@ pub fn spawn_platform(mut commands: Commands, windows: Query<&Window>, textures:
             LockedAxes::ROTATION_LOCKED | LockedAxes::TRANSLATION_LOCKED_Y,
             Ccd::enabled(),
             Restitution::new(0.2),
+            Collider::cuboid(
+                HALF_TILE_SIZE * PLATFORM_DEFAULT_LENGTH as f32,
+                HALF_TILE_SIZE,
+            ),
+            GlobalTransform::default(),
+            Transform::from_xyz(0.0, -SCREEN_HEIGHT / 2.0 + TILE_SIZE, 1.0),
         ))
         .with_children(|children| {
             children.spawn((
-                Collider::cuboid(
-                    HALF_TILE_SIZE * PLATFORM_DEFAULT_LENGTH as f32,
-                    HALF_TILE_SIZE - 3.0,
-                ),
-                Transform::from_xyz(
-                    PLATFORM_DEFAULT_LENGTH as f32 * TILE_SIZE / 2.0 - HALF_TILE_SIZE,
-                    0.0,
-                    0.0,
-                ),
+                SpriteBundle {
+                    texture: textures.industrial.texture.clone(),
+                    transform: Transform::from_xyz(
+                        -(PLATFORM_DEFAULT_LENGTH as f32) * TILE_SIZE / 2.0 + HALF_TILE_SIZE,
+                        0.0,
+                        0.0,
+                    ),
+                    ..Default::default()
+                },
+                TextureAtlas {
+                    layout: textures.industrial.layout.clone(),
+                    index: PLATFORM_FIRST_TILE,
+                },
             ));
         })
         .id();
@@ -71,7 +66,12 @@ pub fn spawn_platform(mut commands: Commands, windows: Query<&Window>, textures:
             .spawn((
                 SpriteBundle {
                     texture: textures.industrial.texture.clone(),
-                    transform: Transform::from_xyz(TILE_SIZE * i as f32, 0.0, 0.0),
+                    transform: Transform::from_xyz(
+                        TILE_SIZE * i as f32 - PLATFORM_DEFAULT_LENGTH as f32 * TILE_SIZE / 2.0
+                            + HALF_TILE_SIZE,
+                        0.0,
+                        0.0,
+                    ),
                     ..Default::default()
                 },
                 TextureAtlas {
@@ -89,7 +89,7 @@ pub fn spawn_platform(mut commands: Commands, windows: Query<&Window>, textures:
             SpriteBundle {
                 texture: textures.industrial.texture.clone(),
                 transform: Transform::from_xyz(
-                    TILE_SIZE * (PLATFORM_DEFAULT_LENGTH - 1) as f32,
+                    PLATFORM_DEFAULT_LENGTH as f32 * TILE_SIZE / 2.0 - HALF_TILE_SIZE,
                     0.0,
                     0.0,
                 ),
@@ -99,18 +99,16 @@ pub fn spawn_platform(mut commands: Commands, windows: Query<&Window>, textures:
                 layout: textures.industrial.layout.clone(),
                 index: PLATFORM_LAST_TILE,
             },
-            Restitution::new(0.2),
         ))
         .id();
     commands.entity(platform_entity).add_child(last_tile_entity);
-    spawn_ball_on_platform(commands, textures, platform_entity, PLATFORM_DEFAULT_LENGTH);
+    spawn_ball_on_platform(commands, textures, platform_entity);
 }
 
 pub fn spawn_ball_on_platform(
     mut commands: Commands,
     textures: Res<Textures>,
     platform_entity: Entity,
-    platform_length: u32,
 ) {
     let ball_entity = commands
         .spawn((
@@ -120,11 +118,7 @@ pub fn spawn_ball_on_platform(
             },
             SpriteBundle {
                 texture: textures.ball.clone(),
-                transform: Transform::from_xyz(
-                    platform_length as f32 * TILE_SIZE / 2.0 - HALF_TILE_SIZE,
-                    BALL_SPRITE_RADIUS + HALF_TILE_SIZE,
-                    3.0,
-                ),
+                transform: Transform::from_xyz(0.0, BALL_SPRITE_RADIUS + HALF_TILE_SIZE, 3.0),
                 ..Default::default()
             },
             RigidBody::Dynamic,
@@ -137,6 +131,7 @@ pub fn spawn_ball_on_platform(
                 impulse: Vec2::new(0.0, 0.0),
                 torque_impulse: 0.0,
             },
+            ActiveEvents::COLLISION_EVENTS,
             Ccd::enabled(),
             Restitution::new(2.0),
         ))
@@ -148,9 +143,8 @@ pub fn move_platform(
     mut query: Query<(
         &mut ExternalImpulse,
         &mut Velocity,
-        &GlobalTransform,
-        &Platform,
-    )>,
+        &GlobalTransform
+    ), With<Platform>>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     window: Query<&Window, With<PrimaryWindow>>,
     camera_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
@@ -162,7 +156,7 @@ pub fn move_platform(
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate());
 
-    for (mut binding, mut velocity, transform, platform) in query.iter_mut() {
+    for (mut binding, mut velocity, transform) in query.iter_mut() {
         let force = binding.as_mut();
         if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
             force.impulse = Vec2::new(-30000.0, 0.0);
@@ -172,9 +166,7 @@ pub fn move_platform(
         }
 
         if let Some(cursor_position) = cursor_position {
-            let diff = cursor_position.x
-                - transform.translation().x
-                - get_plarform_center_x_offset(platform.length);
+            let diff = cursor_position.x - transform.translation().x;
             velocity.linvel = Vec2::new(diff * 10.0, 0.0);
         }
     }
@@ -189,11 +181,55 @@ pub fn despawn_platform(mut commands: Commands, platform_query: Query<Entity, Wi
 pub fn new_ball_check(
     commands: Commands,
     textures: Res<Textures>,
-    platform_query: Query<(Entity, &Platform)>,
+    platform_query: Query<Entity, With<Platform>>,
     mut new_ball_events: EventReader<NewBallOnPlatform>,
 ) {
     if new_ball_events.read().count() > 0 {
-        let (platform_entity, platform) = platform_query.single();
-        spawn_ball_on_platform(commands, textures, platform_entity, platform.length);
+        let platform_entity = platform_query.single();
+        spawn_ball_on_platform(commands, textures, platform_entity);
     }
+}
+
+pub fn collision_handler(
+    mut commands: Commands,
+    mut collisions: EventReader<CollisionEvent>,
+    platform_query: Query<(Entity, &Platform)>,
+    ball_query: Query<(Entity, &Ball)>,
+
+) {
+    for ev in collisions.read() {
+        if let CollisionEvent::Started(entity1, entity2, _) = ev {
+            if platform_query.get(*entity1).is_ok() && ball_query.get(*entity2).is_ok() {
+                if let Ok((_, ball)) = ball_query.get(*entity2) {
+                    if ball.lay_on_platform{
+                        continue;
+                    }
+                if let Ok((_, platform)) = platform_query.get(*entity1) {
+                    commands.spawn(AudioBundle{
+                        source: platform.collision_sound.clone(),
+                        settings: PlaybackSettings{
+                            mode:bevy::audio::PlaybackMode::Despawn,
+                            ..Default::default()
+                        }
+                    });
+                }
+            }
+            if platform_query.get(*entity2).is_ok() && ball_query.get(*entity1).is_ok() {
+                if let Ok((_, ball)) = ball_query.get(*entity1) {
+                    if ball.lay_on_platform{
+                        continue;
+                    }
+                if let Ok((_, platform)) = platform_query.get(*entity2) {
+                    commands.spawn(AudioBundle{
+                        source: platform.collision_sound.clone(),
+                        settings: PlaybackSettings{
+                            mode:bevy::audio::PlaybackMode::Despawn,
+                            ..Default::default()
+                        }
+                    });
+                }
+            }
+        }
+        }
+    }}
 }
